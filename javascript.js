29b1cc7d6aeefcbee384ad7c218a0b99eb7e92d2
Gerardo A. Sorto Chávez <46613301+sortogerardo@users.noreply.github.com>
2019-12-27 16:32:20 -0600

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,48 @@
+var database = [
+    {
+        username: "andrei",
+        password: "supersecret"
+    },
+    {
+        username: "Sally",
+        password: "123"
+    },
+    {
+        username: "ingrid",
+        password: "777"
+    },
+];
+
+var newsFeed = [
+    {
+        username: "Bobby",
+        timeline: "So tired from all that learning"
+    },
+    {
+        username: "Sally",
+        timeline: "Javascript is so cool!"
+    }
+];
+
+function isUserValid(username, password){
+    for(var i=0;i<database.length;i++){
+        if (database[i].username===username &&
+            database[i].password===password ) {
+                return true;
+        }
+    }
+    return false;
+}
+
+var userNamePrompt = prompt("What's your username?");
+var paswordPrompt = prompt("What's your password?");
+
+function signIn(username, password){
+    if (isUserValid(username, password)) {
+        console.log(newsFeed);
+    } else {
+        alert("Sorry, wrong username or password");
+    }
+}
+
+signIn(userNamePrompt, paswordPrompt);
